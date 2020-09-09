d1ff307f164b473045780f93ad0bacf5dad951a6
JaniisKn <janis_kneubuehler@sluz.ch>
2020-09-09 11:05:38 +0200

    Aufgabe 02

--- BlockA_JS/helloWorld.js
+++ BlockA_JS/helloWorld.js
@@ -1,4 +1,7 @@
+for (const key in document) {
+    document.write(key + ":" + document.key + "<br>")
+}
 for (let index = 10; index < 37; index++) {
-    var text = "Schriftgrösse=" + index + "<br>" + "<span style=font-size:" + index + "px>"
+    var text = "<span style=font-size:" + index + "px>" + `Schriftgrösse= ${index} <br>`
     document.write(text);
 }
