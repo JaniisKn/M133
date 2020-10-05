3a99dcafa174f0ea83c62f69a3894ddb6fbf8dd5
Kneubühler Janis [Bison Schweiz AG] <janis.kneubuehler@bison-group.com>
2020-10-05 10:41:47 +0200

    finished Exercise 4

--- Block3C/SchraubenVerkauf.js
+++ Block3C/SchraubenVerkauf.js
@@ -1,5 +1,11 @@
-document
-    .querySelector("send")
-    .addEventListener("click", function() {
-        alert("button clicked");
-    });
+function preisBerechnen() {
+    let preis = 0;
+    preis += 0.2 * document.getElementById("anzahlSchrauben").value;
+    preis += 0.1 * document.getElementById("anzahlMuttern").value;
+    preis += 0.05 * document.getElementById("anzahlUnterlegsscheiben").value;
+    document.getElementById("preis").value = Math.round(preis * 100) / 100;
+    return Math.round(preis * 100) / 100;
+}
+function bestellen() {
+    alert("Bestellung wurde abgesendet! Preis: " + preisBerechnen() + " CHF");
+}
